f3f03ed4542fdb85a79b7709fe073b66bacea9aa
Dario Campagna <campagna@esteco.com>
2016-12-20 19:13:55 +0100

    We now add non-zero positive integers.

--- test/Math/AddFractionTest.php
+++ test/Math/AddFractionTest.php
@@ -29,4 +29,14 @@ class AddFractionTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertEquals(new Fraction(1), (new Fraction(1))->plus(new Fraction(0)));
     }
+
+    /**
+     * @test
+     */
+    public function positiveNonZeroIntegers()
+    {
+        $this->assertEquals(new Fraction(5), (new Fraction(2))->plus(new Fraction(3)));
+    }
+
+
 }
