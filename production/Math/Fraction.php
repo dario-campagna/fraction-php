e74ae313d8a3b415cb302ad262a18fa9e9d639c9
Dario Campagna <campagna@esteco.com>
2016-12-22 11:29:06 +0100

    We now add integers.

--- production/Math/Fraction.php
+++ production/Math/Fraction.php
@@ -6,17 +6,26 @@ namespace Esteco\Math;
 
 class Fraction
 {
+    /*
+     * @var int
+     */
+    private $integer;
 
     /**
      * Fraction constructor.
      * @param int $int
      */
-    public function __construct($int)
+    public function __construct($integer)
     {
+        $this->integer = $integer;
     }
 
     public function plus(Fraction $fraction): Fraction
     {
-        return new Fraction(0);
+        if ($fraction->integer == 0) {
+            return new Fraction(0);
+        } else {
+            return new Fraction($this->integer + $fraction->integer);
+        }
     }
 }
--- test/Math/AddFractionTest.php
+++ test/Math/AddFractionTest.php
@@ -13,4 +13,12 @@ class AddFractionTest extends TestCase
     {
         $this->assertEquals(new Fraction(0), (new Fraction(0))->plus(new Fraction(0)));
     }
+
+    /**
+     * @test
+     */
+    public function negativeIntegers()
+    {
+        $this->assertEquals(new Fraction(2), (new Fraction(-5))->plus(new Fraction(7)));
+    }
 }
